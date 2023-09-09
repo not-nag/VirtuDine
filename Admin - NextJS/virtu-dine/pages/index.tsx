6ba3e3dd2ed64163dd0fdf0b3f22fdde5b022a09
Nagraj Thamankar <nagrajtham@gmail.com>
2023-09-09 10:04:40 +0530

    Initialised NextJS

---
+++ Admin - NextJS/virtu-dine/pages/index.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const TrialPage: React.FC = () => {
+  const message: string = 'This is a trial page with TypeScript!';
+
+  return (
+    <div>
+      <h1>{message}</h1>
+      <p>
+        Hello
+      </p>
+    </div>
+  );
+};
+
+export default TrialPage;
+    
